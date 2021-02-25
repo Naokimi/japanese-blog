3ab76297816d00d063447bdb7c44df368d67e181
Paulo DAlberti <phdalberti@gmail.com>
2021-02-25 23:26:55 +0900

    show page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,11 +4,18 @@ class PostsController < ApplicationController
   end
 
   def show
+    @post = ServerCaller.call('get', "posts/#{params[:id]}")
+    @comments = ServerCaller.call('get', "posts/#{params[:id]}/comments")['comments']
   end
 
   def create
+    response = ServerCaller.call('get', 'posts', params)
+    if response.include?('errors:')
+      render :new
+    else
+      redirect_to post_path
+    end
   end
 
-  def new
-  end
+  def new; end
 end
