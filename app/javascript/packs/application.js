31b4c4c05085f13206b421a383a68cf2b98347ef
Paulo DAlberti <phdalberti@gmail.com>
2021-02-28 00:00:11 +0900

    finish JS

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,26 +12,6 @@ Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 
-// Header background image
-// var header = document.querySelector('#masthead');
-// if (header) {
-//   var headerBg = document.querySelector('#header-bg');
-//   if (headerBg) {
-//     imagesLoaded(headerBg, { background: true }, function () {
-//       header.classList.add('bg--loaded');
-//     });
-//   } else {
-//     header.classList.add('bg--loaded');
-//   }
-// }
-
-// Responsive video embeds
-// var videoEmbeds = [
-//   'iframe[src*="youtube.com"]',
-//   'iframe[src*="vimeo.com"]'
-// ];
-// reframe(videoEmbeds.join(','));
-
 // Mobile menu
 var menuToggle = document.getElementById('menu-toggle');
 if (menuToggle) {
